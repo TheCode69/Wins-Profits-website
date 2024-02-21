c47c2fdc71af336bb8cd5016b84e83c2f4cb1fc2
TheCode69 <dmunene922@gmail.com>
2024-02-21 15:23:14 +0300

    My first Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    const formData = new FormData(this);
+    const email = formData.get('email');
+    const phone = formData.get('phone');
+    const message = formData.get('message');
+    const emailLink = 'mailto:dmune8965@gmail.com' + '?subject=' + encodeURIComponent('Message from ' + email) + '&body=' + encodeURIComponent('Phone Number: ' + phone + '\n\nMessage: ' + message);
+    window.location.href = emailLink;
+});
